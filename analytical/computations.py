c51606e3718c09bcea5b4be7717dce162a833f7e
silencelamb <daixu0113@gmail.com>
2024-07-16 16:29:41 +0800

    update lora computation

--- analytical/computations.py
+++ analytical/computations.py
@@ -1,12 +1,12 @@
 from prettytable import PrettyTable
-TOPS = 1e12
+TFOPs = 1e12
 
-def analyze_training_computation(batch_size, seq_len, vocab_size, hidden_size, head_num, kv_head, immediate_size, layer_num, mlp_style='llama'):
+def analyze_pretrain_computation(batch_size, seq_len, vocab_size, hidden_size, head_num, kv_head, immediate_size, layer_num, mlp_style='llama'):
     '''
-    count and analyze computations of LLM model training
+    count and analyze computations of LLM pretrain/finetune, pretrain or finetune is the same
     Reference:
     'Efficient large-scale language model training on GPU clusters using megatron-LM' 
-    https://dl.acm.org/doi/10.1145/3458817.3476209, page11, APPENDIX: FLOATING-POINT OPERATIONS
+    https://dl.acm.org/doi/10.1145/3458817.3476209, page 11, APPENDIX: FLOATING-POINT OPERATIONS
     
     Parameters:
     - batch_size (int): The batch size.
@@ -20,67 +20,148 @@ def analyze_training_computation(batch_size, seq_len, vocab_size, hidden_size, h
     - mlp_style (str): The style of mlp, 'llama' or 'normal'.
 
     Returns:
-    - int: The total computations (FLOPs).
+    - dict: The computation analysis result.
     '''
-    compuations = {}
+    computations = {}
+    kv_dim = hidden_size // head_num * kv_head
     # forward pass
-    compuations['q_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
-    compuations['k_proj'] = batch_size * 2 * seq_len * hidden_size * (hidden_size // head_num * kv_head)
-    compuations['v_proj'] = batch_size * 2 * seq_len * hidden_size * (hidden_size // head_num * kv_head)
-    compuations['att_q_k'] = batch_size * 2 *  seq_len * seq_len * hidden_size
-    compuations['att_v'] = batch_size * 2 * seq_len * seq_len * hidden_size
-    compuations['o_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
-    compuations['mlp_up'] = batch_size * 2 * seq_len * hidden_size * immediate_size
-    compuations['mlp_down'] = batch_size * 2 * seq_len * immediate_size * hidden_size
+    computations['q_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
+    computations['k_proj'] = batch_size * 2 * seq_len * hidden_size * kv_dim
+    computations['v_proj'] = batch_size * 2 * seq_len * hidden_size * kv_dim
+    computations['att_q_k'] = batch_size * 2 *  seq_len * seq_len * hidden_size
+    computations['att_v'] = batch_size * 2 * seq_len * seq_len * hidden_size
+    computations['o_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
+    computations['mlp_up'] = batch_size * 2 * seq_len * hidden_size * immediate_size
+    computations['mlp_down'] = batch_size * 2 * seq_len * immediate_size * hidden_size
     
     if mlp_style == 'llama':        
-        compuations['mlp_gate'] = batch_size * 2 * seq_len * hidden_size * immediate_size
+        computations['mlp_gate'] = batch_size * 2 * seq_len * hidden_size * immediate_size
     else:
-        compuations['mlp_gate'] = 0
+        computations['mlp_gate'] = 0
     # multiply layer_num
-    for k, v in compuations.items():
-        compuations[k] = v * layer_num
+    for k, v in computations.items():
+        computations[k] = v * layer_num
 
     # add lm_head
-    compuations['lm_head'] = batch_size * 2 * seq_len * hidden_size * vocab_size
+    computations['lm_head'] = batch_size * 2 * seq_len * hidden_size * vocab_size
     # backward pass is two times of forward pass
-    for k, v in compuations.items():
-        compuations[k] = 3 * v
-    compuations['total'] = sum(compuations.values())
-    return compuations
+    for k, v in computations.items():
+        computations[k] = 3 * v
+    computations['total'] = sum(computations.values())
+    return computations
+
+
+def analyze_lora_computation(batch_size, seq_len, vocab_size, hidden_size, head_num, \
+    kv_head, immediate_size, layer_num, lora_r, mlp_style='llama'):
+    '''
+    count and analyze computations of LLM lora finetuning
+    Reference:
+    'Efficient large-scale language model training on GPU clusters using megatron-LM' 
+    https://dl.acm.org/doi/10.1145/3458817.3476209, page 11, APPENDIX: FLOATING-POINT OPERATIONS
+    
+    Parameters:
+    - batch_size (int): The batch size.
+    - seq_len (int): The sequence length.
+    - vocab_size (int): The vocabulary size.
+    - hidden_size (int): The hidden size.
+    - head_num (int): The number of attention heads.
+    - kv_head (int): The number of key-value heads.
+    - immediate_size (int): The immediate size, ffn up dim.
+    - layer_num (int): The number of layers.
+    - lora_r (int): The lora_r param.
+    - mlp_style (str): The style of mlp, 'llama' or 'normal'.
+
+    Returns:
+    - dict: The computation analysis result.
+    '''
+    computations = {}
+    kv_dim = hidden_size // head_num * kv_head
+    # forward pass
+    computations['q_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
+    computations['k_proj'] = batch_size * 2 * seq_len * hidden_size * kv_dim
+    computations['v_proj'] = batch_size * 2 * seq_len * hidden_size * kv_dim
+    computations['att_q_k'] = batch_size * 2 *  seq_len * seq_len * hidden_size
+    computations['att_v'] = batch_size * 2 * seq_len * seq_len * hidden_size
+    computations['o_proj'] = batch_size * 2 * seq_len * hidden_size * hidden_size
+
+    computations['lora_q'] = batch_size * 2 * seq_len * hidden_size * lora_r * 2
+    computations['lora_k'] = batch_size * (2 * seq_len * hidden_size * lora_r + 2 * seq_len * lora_r * kv_dim)
+    computations['lora_v'] = batch_size * (2 * seq_len * hidden_size * lora_r + 2 * seq_len * lora_r * kv_dim)
+    computations['lora_o'] = batch_size * 2 * seq_len * hidden_size * lora_r * 2
+    
+    computations['mlp_up'] = batch_size * 2 * seq_len * hidden_size * immediate_size
+    computations['mlp_down'] = batch_size * 2 * seq_len * immediate_size * hidden_size
+    
+    computations['lora_mlp_up'] = batch_size * (2 * seq_len * hidden_size * lora_r + 2 * seq_len * lora_r * immediate_size)
+    computations['lora_mlp_down'] = batch_size * (2 * seq_len * immediate_size * lora_r + 2 * seq_len * lora_r * hidden_size)
+    
+    if mlp_style == 'llama':        
+        computations['mlp_gate'] = batch_size * 2 * seq_len * hidden_size * immediate_size
+        computations['lora_mlp_gate'] = batch_size * (2 * seq_len * hidden_size * lora_r + 2 * seq_len * lora_r * immediate_size)
+    else:
+        computations['lora_mlp_gate'] = computations['mlp_gate'] = 0
+    
+    # multiply layer_num
+    for k, v in computations.items():
+        computations[k] = v * layer_num
+
+    # add lm_head
+    computations['lm_head'] = batch_size * 2 * seq_len * hidden_size * vocab_size
+
+    for k, v in computations.items():
+        if 'lora' in k:
+            # for lora part backward pass is three times of forward pass
+            computations[k] = 3 * v
+        else:
+            # for other part backward pass is two times of forward pass
+            computations[k] = 2 * v
+    computations['total'] = sum(computations.values())
+    return computations
 
 
 if __name__ == '__main__':
 
-    # llama2 7B
+    # llama2 7B, pretrain/SFT
     batch_size, seq_len = 1, 4096
-    print(f'============= llama2-7B ********* batch_size: {batch_size}, seq_len: {seq_len} =============')
-    llama2_compuations = analyze_training_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=32000, hidden_size=4096, \
+    print(f'===== llama2-7B pretrain/SFT********* batch_size: {batch_size}, seq_len: {seq_len} =====')
+    llama2_computations = analyze_pretrain_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=32000, hidden_size=4096, \
         head_num=32, kv_head=32, immediate_size=11008, layer_num=32)
     table = PrettyTable()
-    table.field_names = ["name", "TOPS"]
-    for k, v in llama2_compuations.items():
-        table.add_row([k, f'{v/TOPS: 0.4f}'])
+    table.field_names = ["name", "TFOPs"]
+    for k, v in llama2_computations.items():
+        table.add_row([k, f'{v/TFOPs: 0.4f}'])
     print(table)
         
-    # Qwen2 7B
+    # Qwen2 7B, pretrain/SFT
     batch_size, seq_len = 1, 4096
-    print(f'============= Qwen-2  7B ********* batch_size: {batch_size}, seq_len: {seq_len} =============')
-    qwen2_compuations = analyze_training_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=152064, hidden_size=3584, \
+    print(f'===== Qwen-2  7B pretrain/SFT********* batch_size: {batch_size}, seq_len: {seq_len} =====')
+    qwen2_computations = analyze_pretrain_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=152064, hidden_size=3584, \
         head_num=28, kv_head=4, immediate_size=18944, layer_num=28)
     table = PrettyTable()
-    table.field_names = ["name", "TOPS"]
-    for k, v in qwen2_compuations.items():
-        table.add_row([k, f'{v/TOPS: 0.4f}'])
+    table.field_names = ["name", "TFOPs"]
+    for k, v in qwen2_computations.items():
+        table.add_row([k, f'{v/TFOPs: 0.4f}'])
     print(table)
 
-    # Qwen2 7B
+    # Qwen2 7B, pretrain/SFT
     batch_size, seq_len = 2, 512
-    print(f'============= Qwen-2  7B ********* batch_size: {batch_size}, seq_len: {seq_len} =============')
-    qwen2_compuations = analyze_training_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=152064, hidden_size=3584, \
+    print(f'===== Qwen-2  7B pretrain/SFT********* batch_size: {batch_size}, seq_len: {seq_len} =====')
+    qwen2_computations = analyze_pretrain_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=152064, hidden_size=3584, \
         head_num=28, kv_head=4, immediate_size=18944, layer_num=28)
     table = PrettyTable()
-    table.field_names = ["name", "TOPS"]
-    for k, v in qwen2_compuations.items():
-        table.add_row([k, f'{v/TOPS: 0.4f}'])
+    table.field_names = ["name", "TFOPs"]
+    for k, v in qwen2_computations.items():
+        table.add_row([k, f'{v/TFOPs: 0.4f}'])
     print(table)
+
+    # Qwen2 7B, lora finetuning
+    batch_size, seq_len = 2, 512
+    print(f'===== Qwen-2  7B lora finetuning********* batch_size: {batch_size}, seq_len: {seq_len} =====')
+    qwen2_computations = analyze_lora_computation(batch_size=batch_size, seq_len=seq_len, vocab_size=152064, hidden_size=3584, \
+        head_num=28, kv_head=4, immediate_size=18944, layer_num=28, lora_r=64)
+    table = PrettyTable()
+    table.field_names = ["name", "TFOPs"]
+    for k, v in qwen2_computations.items():
+        table.add_row([k, f'{v/TFOPs: 0.4f}'])
+    print(table)
+
