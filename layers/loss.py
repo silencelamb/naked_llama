da069b18f166fb0e17d1d062108c6c6f8451d7e5
silencelamb <daixu0113@gmail.com>
2024-10-16 18:14:32 +0800

    feat(CrossEntropy): add weight support

--- layers/loss.py
+++ layers/loss.py
@@ -48,14 +48,16 @@ class CrossEntropy:
         log_softmax = torch.log(softmax)
 
         # If weight is provided, (and reduction is not mean!!!)  apply it to the target one-hot encoding
-        if self.reduction != 'mean' and self.weight is not None:
+        if self.reduction == 'mean' and self.weight is not None:
             weight = self.weight.view(1, -1)  # Reshape weight to (1, C)
             target_one_hot = target_one_hot * weight
+        else:
+            weight = torch.ones_like(input)  # Default weight is 1
 
         # Calculate cross entropy loss
         loss = -torch.sum(target_one_hot * log_softmax)
         if self.reduction == 'mean':
-            loss = loss / valid_mask.sum()  # Only scale by the number of valid samples
+            loss = loss / weight[valid_mask].sum()  # Only scale by the number of valid samples
             
         return loss
 
@@ -80,9 +82,11 @@ class CrossEntropy:
         target_one_hot[valid_mask] = target_one_hot[valid_mask].scatter_(1, valid_target.unsqueeze(1), 1)
 
         # If weight is provided, (and reduction is not mean!!!)  apply it to the target one-hot encoding
-        if self.reduction != 'mean' and self.weight is not None:
+        if self.reduction == 'mean' and self.weight is not None:
             weight = self.weight.view(1, -1)  # Reshape weight to (1, C)
             target_one_hot = target_one_hot * weight
+        else:
+            weight = torch.ones_like(input)  # Default weight is 1
             
         # Gradient of the loss w.r.t input, ignoring ignored indices
         grad_input = softmax.clone()
@@ -90,7 +94,7 @@ class CrossEntropy:
         grad_input[~valid_mask] = 0  # Set gradient to zero for ignored indices
 
         if self.reduction == 'mean':
-            grad_input = grad_input / valid_mask.sum()  # Only scale by the number of valid samples
+            grad_input = grad_input / weight[valid_mask].sum()  # Only scale by the number of valid samples
 
         return grad_input
 
