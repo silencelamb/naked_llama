45d0e7206340604f6a38d6cb2353abe7fc2229c7
silencelamb <daixu0113@gmail.com>
2024-11-19 17:46:40 +0000

    add convert qwen2 to llama hf model scripts

---
+++ hf_example/convert_qwen_to_llama_hf.py
@@ -0,0 +1,50 @@
+from transformers import LlamaConfig, AutoModelForCausalLM, LlamaForCausalLM
+import torch
+
+# 加载 Qwen2 模型
+qwen_model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen2-7B-Instruct",
+    torch_dtype="auto",
+    device_map="auto"
+)
+
+# 创建 LLaMA2 格式的配置
+llama_config = LlamaConfig(
+    vocab_size=qwen_model.config.vocab_size,
+    hidden_size=qwen_model.config.hidden_size,
+    intermediate_size=qwen_model.config.intermediate_size,
+    num_hidden_layers=qwen_model.config.num_hidden_layers,
+    num_attention_heads=qwen_model.config.num_attention_heads,
+    num_key_value_heads=qwen_model.config.num_key_value_heads,
+    max_position_embeddings=qwen_model.config.max_position_embeddings,
+    rope_theta=qwen_model.config.rope_theta,
+    rms_norm_eps=qwen_model.config.rms_norm_eps,
+    attention_bias=True, # Qwen2-7B-Instruct 有 Q K V的bias,但是又没有O的bias,所以后面还需要单独对O的bias进行置0处理
+    torch_dtype=qwen_model.config.torch_dtype,
+    bos_token_id=qwen_model.config.bos_token_id,
+    eos_token_id=qwen_model.config.eos_token_id,
+)
+
+# 转换模型权重
+llama_model = LlamaForCausalLM(llama_config).to(dtype=torch.bfloat16).to(device=qwen_model.device)
+
+# 复制生成配置，这个挺重要的，因为生成配置会影响到模型的输出对齐
+# 关键配置比如是否sample，是否repetition_penalty等
+llama_model.generation_config = qwen_model.generation_config
+
+# 复制权重(这里需要根据具体层的对应关系进行修改)
+for name, param in qwen_model.named_parameters():
+    if name in llama_model.state_dict():
+        llama_model.state_dict()[name].copy_(param)
+    else:
+        print(f"Layer {name} not found in LlamaForCausalLM.")
+
+# 将 self_attn.o_proj.bias 设置为 0，因为没办法单独设置o的biase为False
+# 其实可以修改下config单独配置o的bias，这样就不用手动设置了，也不会有额外计算
+for name, param in llama_model.named_parameters():
+    if 'self_attn.o_proj.bias' in name:
+        print(f'set {name} to zero')
+        llama_model.state_dict()[name].copy_(torch.zeros_like(param))
+
+# 保存转换后的模型
+llama_model.save_pretrained("qwen2_converted_llama")
--- hf_example/hf_qwen2_7b.py
+++ hf_example/hf_qwen2_7b.py
@@ -1,18 +1,13 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM
 device = "cuda" # the device to load the model onto
-
-model = AutoModelForCausalLM.from_pretrained(
-    "Qwen/Qwen2-7B-Instruct",
-    torch_dtype="auto",
-    device_map="auto"
-)
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
+attn_impl = 'eager' # the attention implementation to use
 
 prompt = "Give me a short introduction to large language model."
 messages = [
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": prompt}
 ]
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
 text = tokenizer.apply_chat_template(
     messages,
     tokenize=False,
@@ -20,31 +15,41 @@ text = tokenizer.apply_chat_template(
 )
 model_inputs = tokenizer([text], return_tensors="pt").to(device)
 
-generated_ids = model.generate(
-    model_inputs.input_ids,
-    max_new_tokens=32
-)
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+# Qwen model
+qwen_model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen2-7B-Instruct", 
+    torch_dtype='auto',
+    attn_implementation=attn_impl).cuda()
+qwen_generated_ids = qwen_model.generate(model_inputs.input_ids, max_new_tokens=32, do_sample=False)
+qwen_generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, qwen_generated_ids)
+]
+print('Original Qwen2-7B-Instruct:')
+print(qwen_generated_ids)
+qwen_response = tokenizer.batch_decode(qwen_generated_ids, skip_special_tokens=True)[0]
+print(qwen_response)
+
+# Converted LlaMA model
+llama_model = LlamaForCausalLM.from_pretrained(
+    "qwen2_converted_llama",
+    torch_dtype='auto',
+    attn_implementation=attn_impl).cuda()
+llama_generated_ids = llama_model.generate(model_inputs.input_ids, max_new_tokens=32, do_sample=False)
+llama_generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, llama_generated_ids)
 ]
 
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+# detail result to compare logits
+qwen_detail_output = qwen_model(input_ids = model_inputs.input_ids, output_hidden_states = True, return_dict = True)
+llama_detail_output = llama_model(input_ids = model_inputs.input_ids, output_hidden_states = True, return_dict = True)
 
-print(response)
+print('Converted Llama:')
+print(llama_generated_ids)
+llama_response = tokenizer.batch_decode(llama_generated_ids, skip_special_tokens=True)[0]
+print(llama_response)
 
+print(f'total generated {len(qwen_generated_ids[0])} tokens')
+print(f'generated_ids equal: {(qwen_generated_ids[0] == llama_generated_ids[0]).all()}')
+print(f'response equal: {qwen_response == llama_response}')
 
-model = LlamaForCausalLM.from_pretrained(
-    "Qwen/Qwen2-7B-Instruct",
-    torch_dtype="auto",
-    device_map="auto"
-)
-generated_ids = model.generate(
-    model_inputs.input_ids,
-    max_new_tokens=32
-)
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
-
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+print(f'logits equal: {(qwen_detail_output["logits"] == llama_detail_output["logits"]).all()}')
