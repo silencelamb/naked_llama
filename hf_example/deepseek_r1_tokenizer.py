e9e42ff0d85441e238063623d0d8d453695397a9
silencelamb <daixu0113@gmail.com>
2025-08-01 10:45:35 +0800

    test <think> <\think>

--- hf_example/deepseek_r1_tokenizer.py
+++ hf_example/deepseek_r1_tokenizer.py
@@ -6,10 +6,10 @@ tokenizer = AutoTokenizer.from_pretrained('deepseek-ai/DeepSeek-R1', trust_remot
 messages = [
     # {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": "Hello, how are you?"},
-    {"role": "assistant", "content": "I'm doing great. How can I help you today?"},
-    {"role": "user", "content": "Can you tell me a joke?"},
-    {"role": "assistant", "content": "bla bla bla, 讲完了, 好不好笑"},
-    {"role": "user", "content": "一点也不好笑。。。 还是帮我写代码吧"},
+    {"role": "assistant", "content": "<think>一些思考 bla bla bla bla</think> I'm doing great. How can I help you today?"},
+    {"role": "user", "content": " Can you tell me a joke?"},
+    {"role": "assistant", "content": "<think>一些思考 bla bla bla bla</think> bla bla bla, 讲完了, 好不好笑"},
+    {"role": "user", "content": " 一点也不好笑。。。 还是帮我写代码吧"},
 ]
 
 # 应用 chat template，add_generation_prompt=True 表示最后会加 assistant 的回答提示
